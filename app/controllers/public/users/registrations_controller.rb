621f4d17e91fd014a25d3b614fb22be0cd59885f
maruyu31 <you3746336@gmail.com>
2021-08-29 07:47:54 +0000

    [fix] インデントの修正

--- app/controllers/public/users/registrations_controller.rb
+++ app/controllers/public/users/registrations_controller.rb
@@ -3,11 +3,11 @@
 class Public::Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
-  before_action :ensure_normal_user, only: [:update, :cancel]
+  before_action :ensure_normal_user, only: [:update, :unsubscribe]
 
   def ensure_normal_user
     if resource.email == 'guest@example.com'
-      redirect_to root_path
+      redirect_to root_path, alert: "ゲストユーザーの編集はできません"
     end
   end
 
