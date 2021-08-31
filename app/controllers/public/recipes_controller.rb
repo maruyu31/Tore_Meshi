5ed2bf202ead5e64c418499497c93162ed1cc982
maruyu31 <you3746336@gmail.com>
2021-08-31 01:35:38 +0000

    [Fix] ログイン前のレシページ閲覧機能を修正

--- app/controllers/public/recipes_controller.rb
+++ app/controllers/public/recipes_controller.rb
@@ -1,5 +1,5 @@
 class Public::RecipesController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
 
   def new
     @recipe = Recipe.new
