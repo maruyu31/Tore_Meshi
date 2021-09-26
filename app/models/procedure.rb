9b49751d708f7b945504cac16610fe3e7e289f05
maruyu31 <you3746336@gmail.com>
2021-09-26 14:08:18 +0000

    [Add] モデルテストの追加

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,5 +1,6 @@
 class Procedure < ApplicationRecord
   belongs_to :recipe
 
+  validates :recipe_id, presence: true
   validates :body, presence: true
 end
--- spec/factories/favorite.rb
+++ spec/factories/favorite.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :favorite do
-    association :user
     association :recipe
+    user { recipe.user }
   end
 end
