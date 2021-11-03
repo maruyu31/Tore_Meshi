89266d24e592b5e4b9650c30a05fb3e44a4c13cc
maruyu31 <you3746336@gmail.com>
2021-11-03 15:04:48 +0000

    [Fix] Recaptureの修正

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.site_key  = Rails.application.credentials.dig(:recaptcha_site_key)
-  config.secret_key  = Rails.application.credentials.dig(:recaptcha_secret_key)
+  config.site_key   = Rails.application.credentials.aws[:recaptcha_site_key]
+  config.secret_key = Rails.application.credentials.aws[:recaptcha_secret_key]
 end
